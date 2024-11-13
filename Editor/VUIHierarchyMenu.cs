1b80e9bd91851ca69b635f4134cbc500fecd7591
Luke Matakitoga <luke.matakitoga@cetsat.com>
2024-11-13 19:00:00 +0000

    Added panel and dropdown

--- Editor/VUIHierarchyMenu.cs
+++ Editor/VUIHierarchyMenu.cs
@@ -9,7 +9,7 @@ public class VUIHierarchyMenu : MonoBehaviour
         GameObject prefab = Resources.Load<GameObject>("Prefabs/VUIButton");
 
         GameObject instance = GameObject.Instantiate(prefab);
-        instance.name = instance.name.Replace("(Clone)", "");
+        instance.name = "VUI Button";
 
         Undo.RegisterCreatedObjectUndo(instance, "Create " + instance.name);
 
@@ -24,7 +24,41 @@ public class VUIHierarchyMenu : MonoBehaviour
         GameObject prefab = Resources.Load<GameObject>("Prefabs/VUITextBox");
 
         GameObject instance = GameObject.Instantiate(prefab);
-        instance.name = instance.name.Replace("(Clone)", "");
+        instance.name = "VUI TextBox";
+
+        Undo.RegisterCreatedObjectUndo(instance, "Create " + instance.name);
+
+        GameObjectUtility.SetParentAndAlign(instance, menuCommand.context as GameObject);
+
+        Selection.activeObject = instance;
+    }
+    
+    [MenuItem("GameObject/UI/VUI/Dropdown", false, 0)]
+    public static void AddVUIDropdown(MenuCommand menuCommand)
+    {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/VUIDropdown");
+
+        GameObject instance = GameObject.Instantiate(prefab);
+        instance.name = "VUI Dropdown";
+
+        Undo.RegisterCreatedObjectUndo(instance, "Create " + instance.name);
+
+        GameObjectUtility.SetParentAndAlign(instance, menuCommand.context as GameObject);
+
+        Selection.activeObject = instance;
+    }
+    
+    [MenuItem("GameObject/UI/VUI/Panel", false, 0)]
+    public static void AddVUIPanel(MenuCommand menuCommand)
+    {
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/VUIPanel");
+
+        GameObject instance = GameObject.Instantiate(prefab);
+        instance.name = "VUI Panel";
+
+        int panelId = EditorPrefs.GetInt("VUI_PanelIDCount", 0);
+        instance.GetComponent<VUIPanel>().PanelId = $"panel_{panelId}";
+        EditorPrefs.SetInt("VUI_PanelIDCount", panelId+1);
 
         Undo.RegisterCreatedObjectUndo(instance, "Create " + instance.name);
 
